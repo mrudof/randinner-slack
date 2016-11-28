e1cfc9dec3db1133365a15778d6ed8e88bca1531
mrudof <melissarudofsky@gmail.com>
2016-11-27 21:05:16 -0500

    add logic for parsing slack response

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-// var request = require('request');
 var app = express();
 
 var bodyParser = require('body-parser');
@@ -14,23 +13,18 @@ app.all('*', function(req, res, next) {
 });
 
 app.post('/', function (req, res) {
-  // var queryTerm = req.params.query_term;
   res.setHeader('Content-Type', 'application/json');
-  // res.end(queryTerm);
-  console.log(req.body);
-  res.end(req.body);
+
+  var slackBody = req.body;
+  console.log(slackBody);
+
+  var text = slackBody.text.split(" ");
+  var cuisine = text[0];
+  var location = text.slice(1,text.length).join(" ");
+
+  res.end("You want to eat " + cuisine + " in or around the location: " + location);
 })
 
-
-// app.get('/:query_term', function (req, res) {
-//   var api_url = "http://api-3283.iheart.com/api/v1/catalog/searchAll?keywords=" + req.params.query_term + "&queryTrack=false&queryBundle=false&queryArtist=true&queryStation=false&queryFeaturedStation=false&queryTalkShow=false&queryTalkTheme=false&queryKeyword=false&countryCode=US"
-//   request(api_url, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       res.setHeader('Content-Type', 'application/json');
-//       res.end(body)
-//     }
-//   })
-// })
 
 var server = app.listen(process.env.PORT, function () {
   var host = server.address().address
