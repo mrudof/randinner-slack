709fba10d0bfd374271517c6ec7ba1559ed40fbe
mrudof <melissarudofsky@gmail.com>
2016-12-25 20:02:51 -0500

    add restaurant filter to api call

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/', function (req, res) {
   var location = text[text.length - 1];
   var name, url, rating;
 
-  yelp.search({ term: `${cuisine}`, location: `${location}`, sort: '2', limit: '40', radius_filter: '3000' })
+  yelp.search({ term: `${cuisine}`, location: `${location}`, sort: '2', limit: '40', radius_filter: '3000', category_filter: 'restaurants'})
     .then((data) => {
     var businesses = data.businesses;
     if (businesses.length > 0) {
